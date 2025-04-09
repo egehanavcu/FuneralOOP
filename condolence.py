01f0450cb0905fcd3a6de94ef716811b547106a4
Egehan <egehanavcu@gmail.com>
2025-04-09 22:29:27 +0200

    Removed CondolenceMessage class

--- condolence.py
+++
@@ -1,7 +0,0 @@
-class CondolenceMessage:
-    def __init__(self, sender, message):
-        self.sender = sender
-        self.message = message
-
-    def __str__(self):
-        return f'{self.sender.name} said: "{self.message}"'
