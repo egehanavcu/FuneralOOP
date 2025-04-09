fc1adc97b6f62cc3c4b71cbea2dd1cd5e353034c
Egehan <egehanavcu@gmail.com>
2025-04-09 19:17:53 +0200

    Added Gassal (Funeral Washer) class with Person inheritance

---
+++ models/gassal.py
@@ -0,0 +1,9 @@
+from models.person import Person
+
+class Gassal(Person):
+    def __init__(self, name, age, experience_years):
+        super().__init__(name, age)
+        self.experience_years = experience_years
+
+    def wash_body(self):
+        return f"{self.name} ({self.experience_years} years of experience) washed the body."
