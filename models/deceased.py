3681abfe7a8de2f2196bb451206dc6c95f908b1c
Egehan <egehanavcu@gmail.com>
2025-04-09 19:08:51 +0200

    Added Deceased class with Person inheritance

---
+++ models/deceased.py
@@ -0,0 +1,7 @@
+from models.person import Person
+
+class Deceased(Person):
+    def __init__(self, name, age, date_of_death, cause_of_death):
+        super().__init__(name, age)
+        self.date_of_death = date_of_death
+        self.cause_of_death = cause_of_death
