570f72db17ce506cd0d1d4fe0ba084498a3a61b5
Egehan <egehanavcu@gmail.com>
2025-04-09 19:45:15 +0200

    Created instances of classes

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from models.deceased import Deceased
+from models.family_member import FamilyMember
+from models.imam import Imam
+from models.gassal import Gassal
+
 if __name__ == "__main__":
-    pass
+    deceased = Deceased("Ahmet Yılmaz", 75, "09.04.2025", "Heart Attack")
+    family = [
+        FamilyMember("Ayşe Yılmaz", 73, "Wife"),
+        FamilyMember("Mehmet Yılmaz", 50, "Son"),
+        FamilyMember("Fatma Kaya", 48, "Daughter")
+    ]
+    imam = Imam("Hüseyin Hodja", 55, "Istanbul Mosque")
+    gassal = Gassal("Eda Gassal", 40, 11)
