30b323781b95569db6ee4bfd15528649433f7d35
Egehan <egehanavcu@gmail.com>
2025-04-09 22:40:58 +0200

    Splitted hold_funeral into multiple functions

--- funeral.py
+++ funeral.py
@@ -6,14 +6,22 @@ class Funeral:
         self.services = services
 
     def hold_funeral(self):
+        self._print_deceased_info()
+        self._print_attendees()
+        self._print_condolences()
+        self._perform_services()
+        self._print_total_cost()
+
+    def _print_deceased_info(self):
         print("--- Deceased Information ---")
         print(f"Deceased Name: {self.deceased.name}")
         print(f"Date of Death: {self.deceased.date_of_death}")
         print(f"Cause of Death: {self.deceased.cause_of_death}")
         print(f"Funeral Cemetery: {self.cemetery.name} - {self.cemetery.address}")
 
-        relatives = [attendee for attendee in self.attendees if attendee.is_relative]
-        non_relatives = [attendee for attendee in self.attendees if not attendee.is_relative]
+    def _print_attendees(self):
+        relatives = [a for a in self.attendees if a.is_relative]
+        non_relatives = [a for a in self.attendees if not a.is_relative]
 
         print("\n--- Family Members ---")
         for member in relatives:
@@ -23,14 +31,17 @@ class Funeral:
         for member in non_relatives:
             print(f"Name: {member.name}, Age: {member.age}, Relation: {member.relation}")
 
+    def _print_condolences(self):
         print("\n--- Condolence Messages ---")
         for attendee in self.attendees:
             print(attendee.say_condolence())
-            
+
+    def _perform_services(self):
         print("\n--- Services ---")
         for service in self.services:
             print(service.perform())
 
+    def _print_total_cost(self):
         print("\nTotal Cost of the Funeral:", self.calculate_total_cost(), "Turkish Liras.")
 
     def calculate_total_cost(self):
